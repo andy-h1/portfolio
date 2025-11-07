177d89c5120096039393217e8252e0a790891dea
andy-h1 <a.huynh2612@gmail.com>
2025-11-07 11:22:23 +0000

    fix: updating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -275,28 +275,19 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            Software Engineer, ex-neonatal nurse, and amateur fitness fanatic.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I'm Andy, a software engineer and ex-neonatal nurse based in London. I specialise in building out beautiful user interfaces using modern front-end technologies and staying up-to-date with current tech trends.
           </p>
           <div className="mt-6 flex gap-6">
-            <SocialLink href="#" aria-label="Follow on X" icon={XIcon} />
             <SocialLink
-              href="#"
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
-              href="#"
+              href="https://github.com/andy-h1"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="#"
+              href="https://www.linkedin.com/in/andyhuynh1/"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
