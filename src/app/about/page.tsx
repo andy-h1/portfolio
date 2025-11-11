b250414d05deea7d86dc4b0c5221c03ffde9313c
andy-h1 <a.huynh2612@gmail.com>
2025-11-11 18:16:03 +0000

    refactor: update bio

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -73,45 +73,36 @@ export default function About() {
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+             For as long as I remember I've found joy in building things—in another life I'm sure I would have been an architect. 
+             I love obsessing over finer details and seeing how complex things can be broken down into small building blocks. 
+             It probably makes sense then how I eventually became a software engineer.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              Before tech, I wore several different hats, most notably as a neonatal nurse in London hospitals. How did I end up in nursing?
+              The truth is I was a bit all over the place as a teenager and loved the idea of helping people—which I still do, just through tech now. 
+              Would I do things differently given another chance? Absolutely not. 
+              I loved the hustle and bustle of it all, everyone coming together to give the best care for their patients. Unforgettable.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              The transition from nursing to tech might seem dramatic, but both share something: that sense of magic when everything clicks. I felt it as a kid watching my uncle code, I feel it now, and I love helping others discover it too.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              If you don't find me training for some physical event (I raise money for charities each year by putting my body through some form of pain), you can find me working on side projects, learning new technologies in the ever-changing world of tech, and trying to recreate that <em>magic</em> feeling myself!
             </p>
           </div>
         </div>
         <div className="lg:pl-20">
           <ul role="list">
-            <SocialLink href="#" icon={XIcon}>
+            {/* <SocialLink href="#" icon={XIcon}>
               Follow on X
             </SocialLink>
             <SocialLink href="#" icon={InstagramIcon} className="mt-4">
               Follow on Instagram
-            </SocialLink>
-            <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+            </SocialLink> */}
+            <SocialLink href="https://github.com/andy-h1" icon={GitHubIcon} className="mt-4">
               Follow on GitHub
             </SocialLink>
-            <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+            <SocialLink href="https://www.linkedin.com/in/andyhuynh1/" icon={LinkedInIcon} className="mt-4">
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
